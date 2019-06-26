642f1a20a02c6775a3562fd09a92cc6b379c4cc5
Garrett Mosier <garrettmosier@gmail.com>
2019-06-26 12:06:52 -0700

    Added dummy analytic functions

--- src/Main.hs
+++ src/Main.hs
@@ -18,6 +18,29 @@ fileLocation = "foo.txt"
 postData :: PostRequest -> IO ()
 postData pr@(PostRequest ts uid event) = appendFile fileLocation ((show pr) ++ "\n")
 
+readStorage :: IO [PostRequest]
+readStorage = return []
+
+getDataPure :: TimeStamp -> [PostRequest] -> Response
+getDataPure ts requests = Response (length counts) clicks impressions 
+               where counts = requests -- Make sure they're unique
+                     clicks = length (filter isClick counts)
+                     impressions = length (filter isImpression counts)
+
+{-
+getDataDirty :: TimeStamp -> Response
+getDataDirty ts = Response (length counts) clicks impressions 
+               where counts = readStorage -- Make sure they're unique
+                     clicks = length (filter isClick counts)
+                     impressions = length (filter isImpression counts)
+-}
+
+isClick (PostRequest _ _ Click) = True
+isClick _ = False
+
+isImpression (PostRequest _ _ Impression) = True
+isImpression _ = False
+
 main :: IO ()
 main = do
   let testPostRequest = PostRequest 1 2 Click
@@ -25,6 +48,9 @@ main = do
 
   postData testPostRequest
   postData testPostRequestTwo
+
+  print $ getDataPure 0 []
+  --print $ getDataDirty []
 
   putStrLn "hello world"
 

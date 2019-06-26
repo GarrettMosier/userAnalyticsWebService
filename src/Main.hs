ce56b569ed01fdabab2035c9bc1ce59e950194cb
Garrett Mosier <garrettmosier@gmail.com>
2019-06-26 11:18:30 -0700

    Added first attempt at base types

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+data Event = Click | Impression deriving Show
+
+type Clicks = Int
+type Impressions = Int
+type UserCount = Int
+data Response = Response UserCount Clicks Impressions deriving Show
+
 main :: IO ()
 main = do
   putStrLn "hello world"

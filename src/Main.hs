3a404a25040c3c5220108e70bb34270345a38aeb
Garrett Mosier <garrettmosier@gmail.com>
2019-06-26 14:25:58 -0700

    Dummy post working

--- src/Main.hs
+++ src/Main.hs
@@ -13,14 +13,15 @@ main = do
   scotty 3000 $ do
     get "/foo" $ do
       text "Sup"
-    get "/analytics" $ do -- TODO Deal with case where timestamp isn't provided
+    get "/analytics" $ do -- TODO Deal with case where timestamp isn't provided.
+      -- TODO Currently fails if foo doesn't exit (which it doesn't on startup)
       timestamp <- param "timestamp" :: ActionM Int
       dataSummary <- liftIO (getDataDirty timestamp) 
       json dataSummary
     post "/analytics" $ do
       timestamp <- param "timestamp"
-      event <- param ":event"
-      user <- param ":user"
+      event <- param "event"
+      user <- param "user"
 
       liftIO (postData (PostRequest timestamp user event)) 
       

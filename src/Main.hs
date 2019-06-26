37ed1120e7321beaa559a05aa23c898bdee0fdc8
Garrett Mosier <garrettmosier@gmail.com>
2019-06-26 11:34:48 -0700

    Writing dummy data to file

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,25 @@ type Impressions = Int
 type UserCount = Int
 data Response = Response UserCount Clicks Impressions deriving Show
 
+type TimeStamp = Int
+
+type UserID = Int
+
+data PostRequest = PostRequest TimeStamp UserID Event deriving Show
+
+fileLocation = "foo.txt"
+
+postData :: PostRequest -> IO ()
+postData pr@(PostRequest ts uid event) = appendFile fileLocation ((show pr) ++ "\n")
+
 main :: IO ()
 main = do
+  let testPostRequest = PostRequest 1 2 Click
+  let testPostRequestTwo = PostRequest 1 2 Impression
+
+  postData testPostRequest
+  postData testPostRequestTwo
+
   putStrLn "hello world"
+
+
